b269fd80067504fe592842841df39b043f001afa
swirling-wind <yaozirui@outlook.com>
2024-06-14 14:13:53 +0100

    Reformat

--- L0146_LRU.cpp
+++ L0146_LRU.cpp
@@ -4,10 +4,11 @@ using namespace boost::ut;
 
 constexpr auto sum(auto... args) { return (args + ...); }
 
-int main() {
-  "sum"_test = [] {
-    expect(sum(0) == 0_i);
-    expect(sum(1, 2) == 3_i);
-    //expect(sum(1, 2) > 0_i and 41_i == sum(40, 2));
-  };
+int main()
+{
+	"sum"_test = []
+	{
+		expect(sum(0) == 0_i);
+		expect(sum(1, 2) == 3_i);
+	};
 }
