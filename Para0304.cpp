6192ab49e642de63f3a2c675d6997f6a48cab98a
swirling-wind <yaozirui@outlook.com>
2024-06-16 10:43:47 +0100

    Finish quick sort

--- Para0304.cpp
+++ Para0304.cpp
@@ -5,7 +5,7 @@
 #include <optional>
 using namespace std;
 
-auto gcd(int a, int b) -> int
+auto gcd(const int a, const int b) -> int
 {
 	if (b == 0)
 	{
@@ -39,10 +39,48 @@ auto binary_search(const vector<int>& arr, const int target) -> optional<size_t>
 	return nullopt;
 }
 
+auto is_sorted(const vector<int>& arr, const size_t start, const size_t end) -> bool
+{
+	for (size_t i = start + 1; i <= end; ++i)
+	{
+		if (arr[i - 1] > arr[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+auto insertion_sort(vector<int>& arr, const ptrdiff_t start, const ptrdiff_t end) -> void
+{
+	for (ptrdiff_t i = start + 1; i <= end; i++)
+	{
+		int key = arr[i];
+		ptrdiff_t j = i - 1;
+		while (j >= start && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = key;
+	}
+}
 
 auto quick_sort_helper(vector<int>& arr, const size_t start, const size_t end) -> void
 {
-	int pivot_value = arr[(start + end) / 2];
+	constexpr size_t min_size = 20;
+
+	if (is_sorted(arr, start, end))
+	{
+		return;
+	}
+	if (end - start < min_size)
+	{
+		insertion_sort(arr, static_cast<ptrdiff_t>(start), static_cast<ptrdiff_t>(end));
+		return;
+	}
+
+	const int pivot_value = arr[(start + end) / 2];
 	auto l = start, r = end;
 	while (l <= r)
 	{
@@ -79,45 +117,11 @@ auto quick_sort(vector<int>& arr) -> void
 	}
 }
 
-auto insertion_sort(vector<int>& arr) -> void
-{
-	for (size_t i = 1; i < arr.size(); ++i)
-	{
-		int key = arr[i];
-		ptrdiff_t j = i - 1;
-		while (j >= 0 && arr[j] > key)
-		{
-			arr[j + 1] = arr[j];
-			j--;
-		}
-		arr[j + 1] = key;
-	}
-}
-
-//void insertion_sort(std::vector<int>& arr, int start, int end) {
-//    for (size_t i = start + 1; i <= end; i++) {
-//        int key = arr[i];
-//        ptrdiff_t j = i - 1;
-//        while (j >= start && arr[j] > key) {
-//            arr[j + 1] = arr[j];
-//            j--;
-//        }
-//        arr[j + 1] = key;
-//    }
-//}
 
 int main()
 {
-	//cout << format("{}\n", gcd(252, 105));
-	//cout << "\nBinary Search\n";
-	//auto arr = vector<int>({1, 2, 3, 4, 6, 7, 8, 9, 10});
-	//cout << binary_search(arr, 1).value_or(-1) << "\n";
-	//cout << binary_search(arr, 10).value_or(-1) << "\n";
-	//cout << binary_search(arr, 3).value_or(-1) << "\n";
-	//cout << binary_search(arr, 5).value_or(-1) << "\n";
-
-	auto unsorted = vector<int>({3, 5, 1});
-	insertion_sort_wrong(unsorted); //, 0, unsorted.size() - 1);
+	auto unsorted = vector<int>({3, 5, 1, 3, 9, 6});
+	quick_sort(unsorted);
 	for (auto i : unsorted)
 	{
 		cout << i << " ";
