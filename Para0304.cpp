b440d7f98890951abace9a84a13f2c5a5e95b716
swirling-wind <yaozirui@outlook.com>
2024-06-16 10:21:58 +0100

    quick sort & insertion sort

--- Para0304.cpp
+++ Para0304.cpp
@@ -5,15 +5,6 @@
 #include <optional>
 using namespace std;
 
-auto factorial(int n) -> int
-{
-	if (n == 1)
-	{
-		return 1;
-	}
-	return factorial(n - 1) * n;
-}
-
 auto gcd(int a, int b) -> int
 {
 	if (b == 0)
@@ -21,27 +12,6 @@ auto gcd(int a, int b) -> int
 		return a;
 	}
 	return gcd(b, a % b);
-}
-
-auto iter_sum(vector<int> arr) -> int
-{
-	if (arr.empty())
-	{
-		return 0;
-	}
-	auto last = arr.back();
-	arr.pop_back();
-	return iter_sum(arr) + last;
-}
-
-auto iter_count(vector<int> arr) -> size_t
-{
-	if (arr.empty())
-	{
-		return 0;
-	}
-	arr.pop_back();
-	return iter_count(arr) + 1;
 }
 
 auto binary_search(const vector<int>& arr, const int target) -> optional<size_t>
@@ -69,37 +39,76 @@ auto binary_search(const vector<int>& arr, const int target) -> optional<size_t>
 	return nullopt;
 }
 
-auto quick_sort(vector<int> arr) -> vector<int>
+
+auto quick_sort_helper(vector<int>& arr, const size_t start, const size_t end) -> void
 {
-	if (arr.size() < 2)
+	int pivot_value = arr[(start + end) / 2];
+	auto l = start, r = end;
+	while (l <= r)
 	{
-		return arr;
-	}	
-	const auto pivot = arr[0];
-	vector<int> less;
-	vector<int> greater;
-	ranges::copy_if(arr.begin() + 1, arr.end(), back_inserter(less),
-	                [pivot](int n) { return n <= pivot; });
-	
-	ranges::copy_if(arr.begin() + 1, arr.end(), back_inserter(greater),
-	                [pivot](int n) { return n > pivot; });
-
-	auto sorted_less = quick_sort(less);
-	auto sorted_greater = quick_sort(greater);
-
-	vector<int> sorted;
-	ranges::copy(sorted_less, back_inserter(sorted));
-	sorted.push_back(pivot);
-	ranges::copy(sorted_greater, back_inserter(sorted));
-	return sorted;
+		while (arr[l] < pivot_value)
+		{
+			++l;
+		}
+		while (arr[r] > pivot_value)
+		{
+			--r;
+		}
+		if (l <= r)
+		{
+			std::swap(arr[l], arr[r]);
+			++l;
+			--r;
+		}
+	}
+	if (start < r)
+	{
+		quick_sort_helper(arr, start, r);
+	}
+	if (l < end)
+	{
+		quick_sort_helper(arr, l, end);
+	}
 }
+
+auto quick_sort(vector<int>& arr) -> void
+{
+	if (arr.size() > 1)
+	{
+		quick_sort_helper(arr, 0, arr.size() - 1);
+	}
+}
+
+auto insertion_sort(vector<int>& arr) -> void
+{
+	for (size_t i = 1; i < arr.size(); ++i)
+	{
+		int key = arr[i];
+		ptrdiff_t j = i - 1;
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = key;
+	}
+}
+
+//void insertion_sort(std::vector<int>& arr, int start, int end) {
+//    for (size_t i = start + 1; i <= end; i++) {
+//        int key = arr[i];
+//        ptrdiff_t j = i - 1;
+//        while (j >= start && arr[j] > key) {
+//            arr[j + 1] = arr[j];
+//            j--;
+//        }
+//        arr[j + 1] = key;
+//    }
+//}
 
 int main()
 {
 	//cout << format("{}\n", gcd(252, 105));
-	//cout << iter_sum(vector<int>({1, 2, 3, 4})) << "\n";
-	//cout << iter_count(vector<int>({2, 3, 1}));
-	//// Test binary_search
 	//cout << "\nBinary Search\n";
 	//auto arr = vector<int>({1, 2, 3, 4, 6, 7, 8, 9, 10});
 	//cout << binary_search(arr, 1).value_or(-1) << "\n";
@@ -107,10 +116,10 @@ int main()
 	//cout << binary_search(arr, 3).value_or(-1) << "\n";
 	//cout << binary_search(arr, 5).value_or(-1) << "\n";
 
-	auto unsorted = vector<int>({3,5,1,2,4});
-	for (auto sorted = quick_sort(unsorted); auto i : sorted)
+	auto unsorted = vector<int>({3, 5, 1});
+	insertion_sort_wrong(unsorted); //, 0, unsorted.size() - 1);
+	for (auto i : unsorted)
 	{
 		cout << i << " ";
 	}
-	
 }
